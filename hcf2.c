f5b0f087ce27ffc0abd47108023ebd7884d99cd3
prince2580 <43826321+prince2580@users.noreply.github.com>
2018-10-19 17:10:38 +0545

    update hcf2.c

--- hcf2.c
+++ hcf2.c
@@ -6,7 +6,7 @@ int hcf(int x, int y);
 
 int main(void)
 {
-	int first, second, temp;
+	int first, second;
 
 	printf("enter two numbers:");
 	scanf("%d %d", &first, &second);
@@ -14,15 +14,7 @@ int main(void)
 	if(first > 0 && second > 0)
 	{
 		
-		if(first > second)
-		{
-			printf("hcf is %d\n", hcf(first, second));
-		}
-		else
-		{
-
-			printf("hcf is %d\n", hcf(second, first));
-		}
+	printf("HCF is %d",hcf(first,second));
 	}
 	else
 	{
@@ -36,23 +28,10 @@ int main(void)
 
 // hcf of two numbers is the largest positive divisior of that two numbers
 int hcf(int x, int y)
-{
-	int quotient, remainder = 1;
-
-	// according to euclid division algorithm one no can be expressed in form of another no as follows
-	// dividend = (divisor * quotient) + remainder
-	// applying division algorithm to divisor and remainder again treating divisor as dividend and remainder as divisor
-	// until remainder is zero and when the remainder is zero the hcf is divisor at that stage
-	while( remainder > 0)
-	{
-		quotient = x / y;
-		remainder = x - (quotient * y);
-		
-		// hcf is y which is a divisior when remainder is zero
-		x = y;
-		y = remainder;
-	}
-			
+{ 
+	if(y!=0)
+	return hcf(y,x%y);
+	else
 	return x;
 }
 
